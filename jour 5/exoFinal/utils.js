303a145fc41084acdab65933f2e6b356dd3449d4
CptMog <djawad.mohamed8@gmail.com>
2023-04-07 14:26:03 +0200

    Jour 5: exoFinal

--- jour 5/exoFinal/utils.js
+++ jour 5/exoFinal/utils.js
@@ -1,5 +1,6 @@
 const dayjs = require('dayjs')
 exports.utils = {
+
     formatDateAllBirth : (students) =>{
         for (let index = 0; index < students.length; index++) {
             students[index].birth = dayjs(students[index].birth).locale('fr').format('DD/MM/YYYY');   
